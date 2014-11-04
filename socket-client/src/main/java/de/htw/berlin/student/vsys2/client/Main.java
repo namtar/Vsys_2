c53dced69687a37e59365749efb965b2fac8ca01
Matthias Drummer <matthias.drummer@Tigerbytes-MBP-03.local>
2014-11-04 18:23:51 +0100

    created packages and package-info files

---
+++ socket-client/src/main/java/de/htw/berlin/student/vsys2/client/Main.java
@@ -0,0 +1,13 @@
+package de.htw.berlin.student.vsys2.client;
+
+/**
+ * Main class for the socket client.
+ * <p/>
+ * Created by matthias.drummer on 04.11.14.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ socket-server/src/main/java/de/htw/berlin/student/vsys2/server/Main.java
@@ -0,0 +1,13 @@
+package de.htw.berlin.student.vsys2.server;
+
+/**
+ * Main class for the socket server.
+ * <p/>
+ * Created by matthias.drummer on 04.11.14.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+}
