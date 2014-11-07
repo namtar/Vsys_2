ddd0a0230dc8823ac3b28d8dff2390889777523d
RonnyRock <ronnytimm@gmx.de>
2014-11-07 10:23:13 +0100

    Client 1.Version

--- socket-both/src/main/java/de/htw/berlin/student/vsys2/both/Client.java
+++ socket-both/src/main/java/de/htw/berlin/student/vsys2/both/Client.java
@@ -1,13 +1,57 @@
 package de.htw.berlin.student.vsys2.both;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.rmi.UnknownHostException;
+
 /**
  * Client that queries the server.
  * <p/>
  * Created by matthias.drummer on 04.11.14.
  */
-public class Client {
 
-	public static void main(String[] args) {
-		
-	}
+public class Client {
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 2) {
+            System.err.println(
+                    "Usage: java Client <host name> <port number>");
+            System.exit(1);
+        }
+
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+
+        try (
+                Socket clientSocket = new Socket(hostName, portNumber);
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            String fromServer;
+            String fromUser;
+
+            while ((fromServer = in.readLine()) != null) {
+                System.out.println("Server: " + fromServer);
+                if (fromServer.equals("quit"))
+                    break;
+
+                fromUser = stdIn.readLine();
+                if (fromUser != null) {
+                    System.out.println("Client: " + fromUser);
+                    out.println(fromUser);
+                }
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                    hostName);
+            System.exit(1);
+        }
+    }
 }
